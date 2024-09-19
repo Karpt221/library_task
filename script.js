e42f8c008a90a4f7b7d1f5724b4ba14420741476
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2024-09-19 20:36:47 +0300

    Add remove and change read status btns

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+
 const booksContainer = document.querySelector('.books-container');
 const addBookBtn = document.querySelector('.add-book-btn');
 const closeAddFormBtn = document.querySelector('.close-add-form');
@@ -9,34 +10,69 @@ const title = document.querySelector("#title");
 const numOfPages = document.querySelector("#numofpages");
 const readStatus = document.querySelector("#readstatus");
 
+
 myLibrary.push(new Book('Harry Potter', 'Joanne Rowling', 242, 'read'),
     new Book('1984', 'George Orwell', 203, 'not read'),
     new Book('It', 'Stephen King', 896, 'read'));
 
 
-function Book(Title, Author, NumOfPages, ReadStatus) {
-    this.Title = Title;
-    this.Author = Author;
-    this.NumOfPages = NumOfPages;
-    this.ReadStatus = ReadStatus;
+function Book(title, author, numOfPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.read = read;
 }
+
+Book.prototype.switchReadStatus = function () {
+    this.read = (this.read === 'read' ? 'not read' : 'read');
+};
 
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
 }
 
-function displayBooks() {
+function getCard(book, index) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+        <p>Title: ${book.title}</p>
+        <p>Author: ${book.author}</p>
+        <p>Number of pages: ${book.numOfPages}</p>
+        <p>Read status: ${book.read}</p>
+        <button class='card-rm-btn' id='${index}'>Remove</button>
+        <button class='card-read-btn' id='${index}'>Change read status</button>
+    `;
+    return card;
+}
+
+function renderBooks() {
     booksContainer.innerHTML = '';
-    myLibrary.forEach(book => {
-        let card =
-            `<div class='card'>
-            <p>Title: ${book.Title}</p>
-            <p>Author: ${book.Author}</p>
-            <p>Number of pages: ${book.NumOfPages}</p>
-            <p>Read status: ${book.ReadStatus}</p>
-        </div>`;
-        booksContainer.innerHTML += card;
+    myLibrary.forEach((book, index) => {
+        booksContainer.prepend(getCard(book, index));
     });
+
+    attachCardBtnListeners();
+}
+
+function attachCardBtnListeners() {
+    document
+        .querySelectorAll('.card-rm-btn')
+        .forEach(btn => {
+            btn.addEventListener('click', () => {
+                myLibrary.splice(Number.parseInt(btn.getAttribute('id')), 1);
+                renderBooks();
+            });
+        });
+
+    document
+        .querySelectorAll('.card-read-btn')
+        .forEach(btn => {
+            btn.addEventListener('click', () => {
+                let bookIndex = Number.parseInt(btn.getAttribute('id'));
+                myLibrary[bookIndex].switchReadStatus();
+                renderBooks();
+            });
+        });
 }
 
 addBookBtn.addEventListener('click', () => {
@@ -57,8 +93,9 @@ document.addEventListener('submit', () => {
     if (readStatus.checked) {
         status = 'read';
     }
-    addBookToLibrary(new Book(title.value, author.value, numOfPages.value, status));
-    displayBooks();
+    let newBook = new Book(title.value, author.value, numOfPages.value, status);
+    addBookToLibrary(newBook);
+    renderBooks();
 });
 
-displayBooks();
+renderBooks();
